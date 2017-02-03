7736eb696a2f7122be7b33cc8bb7ee7308dc6c19
Al Zohali <zohl@fmap.me>
2017-02-03 23:06:47 +0300

    refactored

--- src/Network/Wai/Handler/Warp/SocketActivation.hs
+++ src/Network/Wai/Handler/Warp/SocketActivation.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE GADTs               #-}
 
 {-|
   Module:      Network.Wai.Handler.Warp.SocketActivation
@@ -20,46 +21,28 @@
 module Network.Wai.Handler.Warp.SocketActivation (
     SocketActivationSettings(..)
   , SocketActivationException(..)
+  , isSocketActivationError
   , withSocketActivation
-  , withSocketActivationM
   ) where
 
-import Control.Exception.Base (Exception, bracket)
-import Control.Monad.Catch (MonadThrow(..), catch, throwM)
+import Control.Exception.Base (Exception)
+import Control.Monad.Catch (MonadMask, bracket, MonadThrow, throwM, handleJust)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Default (Default, def)
 import Data.Streaming.Network (bindPortTCP, HostPreference)
-import Data.Typeable (Typeable)
+import Data.Typeable (Typeable, typeOf)
 import Network.Socket (Socket, fdSocket, close)
 import System.Posix.IO (FdOption(CloseOnExec), setFdOption)
 import System.Posix.Internals (setNonBlockingFD)
 import System.Systemd.Daemon (getActivatedSockets)
-  
 
--- | Options that determine activation mechanism.
-data SocketActivationSettings a = SocketActivationSettings {
-    sasPort :: Maybe Int 
-    -- ^ Fallback port to use when the application is started without systemd socket.
-
-  , sasHostPreference :: HostPreference
-    -- ^ Fallback host preference.
-
-  , sasFallbackResult :: Maybe a
-    -- ^ When no sockets are activated, return provided value or throw exception.
-  }
-
-instance Default (SocketActivationSettings a) where
-  def = SocketActivationSettings {
-      sasPort = Nothing
-    , sasHostPreference = "*"
-    , sasFallbackResult = Nothing
-    }
 
 -- | The exception is thrown when something goes wrong with this package.
 data SocketActivationException
      = NoSocketsActivated
      -- ^ Thrown when the application is started without systemd
      -- socket and no fallback port is specified.
+
      | MultipleSocketsActivated Int
      -- ^ Thrown when more than one systemd socket activated the
      -- application.
@@ -68,22 +51,44 @@ data SocketActivationException
 
 instance (Exception SocketActivationException)
 
--- | Wrapper for socket-activated function.
-withSocketActivation :: SocketActivationSettings a -> (Socket -> IO a) -> IO a
-withSocketActivation set f = catch (withSocketActivationM set f) $
-  \(ex :: SocketActivationException) -> case ex of
-     NoSocketsActivated -> maybe (error $ show ex) (return) (sasFallbackResult set)
-     _                  -> error $ show ex
+-- | Exception filter.
+isSocketActivationError :: (Exception e) => e -> Bool
+isSocketActivationError = (typeOf (undefined :: SocketActivationException) ==) . typeOf
+
+
+-- | Options that determine activation mechanism.
+data SocketActivationSettings a = SocketActivationSettings {
+    sasPort :: Maybe Int
+    -- ^ Fallback port to use when the application is started without systemd socket.
+
+  , sasHostPreference :: HostPreference
+    -- ^ Fallback host preference.
+
+  , sasFallbackAction :: SocketActivationException -> IO a
+    -- ^ When no sockets are activated, execute action (if specified) or throw exception.
+  }
+
+instance Default (SocketActivationSettings a) where
+  def = SocketActivationSettings {
+      sasPort = Nothing
+    , sasHostPreference = "*"
+    , sasFallbackAction = throwM
+    }
+
 
 -- | Wrapper for socket-activated function.
-withSocketActivationM :: (MonadIO m, MonadThrow m)
+withSocketActivation :: forall m a. (MonadIO m, MonadMask m)
   => SocketActivationSettings a
-  -> (Socket -> IO a)
+  -> (Socket -> m a)
   -> m a
-withSocketActivationM set f = getSocket set >>= \socket -> liftIO $ bracket (return socket) close f
+withSocketActivation set f = handleJust
+  (\e -> if isSocketActivationError e then Just e else Nothing)
+  (liftIO . sasFallbackAction set)
+  run where
+    run = getSocket set >>= \socket -> bracket (return socket) (liftIO . close) f
 
 
-getSocket :: forall a m. (MonadIO m, MonadThrow m)
+getSocket :: forall m a. (MonadIO m, MonadThrow m)
   => SocketActivationSettings a
   -> m Socket
 getSocket (SocketActivationSettings {..}) = (liftIO getActivatedSockets) >>= \case
@@ -93,9 +98,10 @@ getSocket (SocketActivationSettings {..}) = (liftIO getActivatedSockets) >>= \ca
         sock <- bindPortTCP port sasHostPreference
         setFdOption (fromIntegral $ fdSocket sock) CloseOnExec True
         return sock)
-    
+
   Just [sock] -> liftIO $ do
     setNonBlockingFD (fdSocket sock) True
     return sock
 
   Just ss -> throwM $ MultipleSocketsActivated (length ss)
+
